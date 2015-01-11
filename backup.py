1cf50cd9031d96d715fbbeb8aee3fa7e874d2054
ssstrz <codecats@gmail.com>
2015-01-11 15:43:29 +0100

    changes after test on debian

--- backup.py
+++ backup.py
@@ -10,14 +10,17 @@ class DatabaseExporter(object):
 		if not self.args.database:
 			raise AttributeError('No database selected to backup')
 		port = args.port or '5432'
-		self('pg_dump -U {user} {database} -f {output} -h {host} -p {port}'.format(
+		postfix = ' {}'.format(self.args.additional)
+		command = 'pg_dump -Fc -U {user} {database} -f {output} -h {host} -p {port}'
+		command += postfix
+		self(command.format(
 			user=self.args.user, database=self.args.database, output=self.args.output + '.sql',
 			host=self.args.host, port=port)
 		)
 
 	def mongo(self):
 		port = args.port or '27017'
-		postfix = ''
+		postfix = ' {}'.format(self.args.additional)
 		postfix += ' --db {}'.format(self.args.database) if self.args.database else ''
 		command = 'mongodump --host {host} --port {port} -out {output}'
 		command += postfix
@@ -26,7 +29,10 @@ class DatabaseExporter(object):
 		))
 
 	def __call__(self, command):
-		subprocess.call(command, shell=True)
+		if self.args.generate:
+			print(command)
+		else:
+			subprocess.call(command, shell=True)
 
 if __name__ == '__main__':
 	arg_parser = argparse.ArgumentParser()
@@ -36,6 +42,11 @@ if __name__ == '__main__':
 	arg_parser.add_argument('--output', '-o', type=str, default='output.backup')
 	arg_parser.add_argument('--host', '-ho', type=str, default='localhost')
 	arg_parser.add_argument('--port', '-p', type=str, default=None)
+	arg_parser.add_argument('--generate', '-g', type=bool, default=False, 
+		help='generate command to execute and just print to console')
+	arg_parser.add_argument('--additional', '-a', type=str, default='',
+		help='''additional arguments use this flag with double quote. Example -a "--test true". 
+		If you need only one flag "-t" without argument use space before: -a " -t"''')
 	args = arg_parser.parse_args()
 	exporter = DatabaseExporter(args)
 	for database in args.database_engine.split(','):
--- restore.py
+++ restore.py
@@ -10,14 +10,22 @@ class DatabaseImporter(object):
 		if not self.args.database:
 			raise AttributeError('No database selected to import')
 		port = args.port or '5432'
-		self('psql {database} < {input} -U {user} -h {host} -p {port}'.format(
+		postfix = ' {}'.format(self.args.additional)
+		command = 'pg_restore -c -d {database} -U {user} -h {host} -p {port}'
+		command += postfix
+		command += ' {input}'
+		self(command.format(
 			user=self.args.user, database=self.args.database, input=self.args.input + '.sql',
 			host=self.args.host, port=port)
 		)
+		#self('psql {database} < {input} -U {user} -h {host} -p {port}'.format(
+		#	user=self.args.user, database=self.args.database, input=self.args.input + '.sql',
+		#	host=self.args.host, port=port)
+		#)
 
 	def mongo(self):
 		port = args.port or '27017'
-		postfix = ''
+		postfix = ' {}'.format(self.args.additional)
 		command = 'mongorestore --host {host} --port {port}'
 		command += postfix
 		command += ' {input}'
@@ -26,7 +34,10 @@ class DatabaseImporter(object):
 		))
 
 	def __call__(self, command):
-		subprocess.call(command, shell=True)
+		if self.args.generate:
+			print(command)
+		else:
+			subprocess.call(command, shell=True)
 
 if __name__ == '__main__':
 	arg_parser = argparse.ArgumentParser()
@@ -36,6 +47,11 @@ if __name__ == '__main__':
 	arg_parser.add_argument('--input', '-i', type=str, default='output.backup')
 	arg_parser.add_argument('--host', '-ho', type=str, default='localhost')
 	arg_parser.add_argument('--port', '-p', type=str, default=None)
+	arg_parser.add_argument('--generate', '-g', type=bool, default=False, 
+		help='generate command to execute and just print to console')
+	arg_parser.add_argument('--additional', '-a', type=str, default='',
+		help='''additional arguments use this flag with double quote. Example -a "--test true". 
+		If you need only one flag "-t" without argument use space before: -a " -t"''')
 	args = arg_parser.parse_args()
 	exporter = DatabaseImporter(args)
 	for database in args.database_engine.split(','):
